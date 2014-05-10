7cf0fc01f1024cb4d6ca64637c9d3d49b4aa749d
Matej <picnik@gmail.com>
2014-05-10 14:34:20 +0100

    changed def actn from  site to video, changed app title

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,8 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'WebRec App for PhpDev PwrMtr assmnt',
+	'name'=>'WebRec App for PwrMtr PhpDev assmnt',
+	'defaultController'=>'video',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
--- protected/views/video/index.php
+++ protected/views/video/index.php
@@ -12,7 +12,23 @@ $this->menu=array(
 );
 ?>
 
-<h1>Videos</h1>
+<h1>Record a new video</h1>
+
+<script src='//cameratag.com/2.1/cameratag.js' type='text/javascript'></script>
+<script type='text/javascript'>
+	CameraTag.observe('cam1', 'published', function(){
+		cameraTagForm = document.getElementById(id);
+		cameraTagForm.submit():
+	}) //CameraTag.observe
+</script>
+
+
+<form id='cameraTagForm' action='' method='post'>
+	<camera id='cam1' data-app-id='a-449347e0-b9da-0131-228e-1231390c0c78'></camera>
+</form>	
+
+
+<h1>List of all Videos</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
